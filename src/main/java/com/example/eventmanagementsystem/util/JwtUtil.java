78b3bcfb729f94146469b358879802c472fbce2d
Ali Booresh <alibooresh1936@gmail.com>
2025-01-24 21:51:56 +0330

    Add validateJWT and getUsernameFromJWT to JwtUtil

--- src/main/java/com/example/eventmanagementsystem/util/JwtUtil.java
+++ src/main/java/com/example/eventmanagementsystem/util/JwtUtil.java
@@ -15,12 +15,32 @@ public class JwtUtil {
 
     /**
      * Generating token
+     *
      * @param username User username
      * @return JWT(Token)
      */
     public String generateJWT(String username) {
         return Jwts.builder().setSubject(username).setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+(1000*60*60)))
-                .signWith(SignatureAlgorithm.HS256,SECRET_KEY).compact();
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 60)))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
+
+    public boolean validateJWT(String jwt) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(jwt);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsernameFromJWT(String jwt) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(jwt)
+                .getBody().getSubject();
+    }
+
 }
